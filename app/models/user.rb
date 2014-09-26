a675df61a88933fe5f26671d7ff9ea357e0638c0
saurabh goel <saurabh.goel@housing.com>
2014-09-26 22:06:23 +0530

    deugger changes;

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ActiveRecord::Base
   has_many :songs_queues
   has_many :user_queues
-  
+  before_create :admin_status
+
+  def admin_status
+    admin_users_email = ['saurabhgoel93@gmail.com','gg.gg1947@ymail.com','naman.gemini@gmail.com']
+    self.roles = 1 if admin_users_email.include?(self.email)
+  end
+
   def self.omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
+      user.email = auth.info.email
       user.name = auth.info.name
       user.image = auth.info.image
       user.token = auth.credentials.token
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140926151625) do
+ActiveRecord::Schema.define(version: 20140926162448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20140926151625) do
     t.string   "image"
     t.string   "token"
     t.datetime "expires_at"
+    t.integer  "roles"
   end
 
 end
